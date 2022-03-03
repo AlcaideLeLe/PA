1b28102039352479a0fbe01e923539ac101786c5
AlcaideLeLe <rodrigoalcaide20@gmail.com>
2022-03-03 21:33:05 +0000

    Primeiro

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,6 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	    System.out.println("O pedro é gay " + 123);
+	    System.out.println("O pedro é gayy " + 123);
     }
 }
